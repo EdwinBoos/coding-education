75734c2fa3f9f9e2539718166c54148b66cbca52
InfluenZ <edwin.boos@gmx.de>
2018-04-25 12:02:03 +0200

    Create Log.java

---
+++ java/oop-clock/src/Log.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Edwin Boos
+ * Class {@link Log}
+ *
+ * A Logger class
+ *
+ *
+ */
+public final class Log {
+
+    public Log() { }
+    private String loggedText = "";
+
+    public Log addText(String text) { this.loggedText = this.loggedText + text; return this; }
+
+    public Log addNumber(int number) { this.loggedText = this.loggedText + number; return this; }
+
+    public Log newLine() { this.loggedText = this.loggedText + "\n"; return this; }
+
+    public Log tab() { this.loggedText = this.loggedText + "\t"; return this; }
+
+    public String getLoggedText() { return this.loggedText; }
+
+}
