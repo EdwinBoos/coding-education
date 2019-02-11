b4b78a2a98d167d7b22b215670a36cf394ce4138
INFLUENZ <edwin.boos@gmx.de>
2019-02-11 13:16:25 +0100

    Update ThreadSafeSingletonPattern.java

--- java/patterns/singletonpattern/ThreadSafeSingletonPattern.java
+++ java/patterns/singletonpattern/ThreadSafeSingletonPattern.java
@@ -3,58 +3,54 @@ package org.jsweet;
 import static jsweet.dom.Globals.*;
 
 /**
-* 
-* 
-* A Thread-Safe Singleton Pattern implementation in Java.
-*
-*
-*       
-*      
-* 
-* 
-* 
-* Created by Edwin Boos ( 10.07.2017 )
-* with Jsweet see : http://www.jsweet.org/jsweet-live-sandbox/ 
-* 
-*
-*/
-public class Run 
-{
-	
-	public static void main(String[] args) 
-	{
-	
-		ThreadSafeSingleton singleton1 = ThreadSafeSingleton.getInstance();
-		ThreadSafeSingleton singleton2 = ThreadSafeSingleton.getInstance();
-		
-		// Will log in the console : Is it the same instance? true
-		System.out.println("Is it the same instance? " + ( singleton1 == singleton2 ));
-		
-	}
-	
+ * 
+ * 
+ * A Thread-Safe Singleton Pattern implementation in Java.
+ *
+ *
+ *       
+ *      
+ * 
+ * 
+ * 
+ * Created by Edwin Boos ( 10.07.2017 )
+ * with Jsweet see : http://www.jsweet.org/jsweet-live-sandbox/ 
+ * 
+ *
+ */
+public class Run {
+
+ public static void main(String[] args) {
+
+  ThreadSafeSingleton singleton1 = ThreadSafeSingleton.getInstance();
+  ThreadSafeSingleton singleton2 = ThreadSafeSingleton.getInstance();
+
+  // Will log in the console : Is it the same instance? true
+  System.out.println("Is it the same instance? " + (singleton1 == singleton2));
+
+ }
+
 }
 
 
- 
-public class ThreadSafeSingleton 
-{
- 
-	private static ThreadSafeSingleton instance = null;
- 
-	private ThreadSafeSingleton() {}
- 
-	public static ThreadSafeSingleton getInstance() 
-	{
-		
-		if (instance == null) {
-			synchronized (ThreadSafeSingleton.class) {
-				if (instance == null) {
-					instance = new ThreadSafeSingleton();
-				}
-			}
-		}
-		return instance;
-		
-	}
-	
+
+public class ThreadSafeSingleton {
+
+ private static ThreadSafeSingleton instance = null;
+
+ private ThreadSafeSingleton() {}
+
+ public static ThreadSafeSingleton getInstance() {
+
+  if (instance == null) {
+   synchronized(ThreadSafeSingleton.class) {
+    if (instance == null) {
+     instance = new ThreadSafeSingleton();
+    }
+   }
+  }
+  return instance;
+
+ }
+
 }
