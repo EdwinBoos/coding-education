9cc7d03e4d18d04ac874f5f059cc0ae4f2d26400
INFLUENZ <edwin.boos@gmx.de>
2019-02-11 13:17:56 +0100

    Update Run.java

--- java/oop-clock/src/Run.java
+++ java/oop-clock/src/Run.java
@@ -8,19 +8,18 @@ import java.util.Timer;
  * Run class with the main method.
  *
  */
-public class Run  {
+public class Run {
 
-    
-    public static void main(String[] args)
-    {
 
-        new Timer().schedule(
-                new Clock.Work(
-                        new Clock.Hour(21),
-                        new Clock.Minute(59),
-                        new Clock.Second(45)), 
-                new Clock.Delay(0).getDelay(), 
-                new Clock.Period(1000).getPeriod());
+ public static void main(String[] args) {
 
-    }
+  new Timer().schedule(
+   new Clock.Work(
+    new Clock.Hour(21),
+    new Clock.Minute(59),
+    new Clock.Second(45)),
+   new Clock.Delay(0).getDelay(),
+   new Clock.Period(1000).getPeriod());
+
+ }
 }
