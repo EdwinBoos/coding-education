69c537b37e3b8f82a3288bf0e262d8e7b49d815a
INFLUENZ <edwin.boos@gmx.de>
2019-02-11 13:11:51 +0100

    Update IConstants.java

--- java/patterns/personalpattern/IConstants.java
+++ java/patterns/personalpattern/IConstants.java
@@ -1,47 +1,47 @@
 /**
-* 
-* 
-* An example for storing 'enum' like constants that belong together in only one Interface, by 
-* defining classes in the Interface.
-* 
-* 
-* 
-* Created by Edwin Boos ( 01.06.2017 )
-* with Jsweet see : http://www.jsweet.org/jsweet-live-sandbox/ 
-* 
-*
-*/
+ * 
+ * 
+ * An example for storing 'enum' like constants that belong together in only one Interface, by 
+ * defining classes in the Interface.
+ * 
+ * 
+ * 
+ * Created by Edwin Boos ( 01.06.2017 )
+ * with Jsweet see : http://www.jsweet.org/jsweet-live-sandbox/ 
+ * 
+ *
+ */
 package org.jsweet;
 import static jsweet.dom.Globals.*;
 
 
 public final class Run implements IConstants {
-	
-	public static void main(String[] args) {
-		
-		System.out.println(EnumCar.VW);
-		System.out.println(EnumCar.AUDI);
-		System.out.println(EnumColor.BLACK);
-		System.out.println(EnumColor.WHITE); 
-											
-	}
+
+ public static void main(String[] args) {
+
+  System.out.println(EnumCar.VW);
+  System.out.println(EnumCar.AUDI);
+  System.out.println(EnumColor.BLACK);
+  System.out.println(EnumColor.WHITE);
+
+ }
 }
 
 
 interface IConstants {
-	
-	public final class EnumCar implements IConstants {
-		
-		public final static String VW = "VW";
-		public final static String AUDI = "AUDI";
-		
-	}
-	
-	public final class EnumColor implements IConstants {
-		
-		public final static String BLACK = "schwarz";
-		public final static String WHITE = "weiss";
-		
-	}
-	
+
+ public final class EnumCar implements IConstants {
+
+  public final static String VW = "VW";
+  public final static String AUDI = "AUDI";
+
+ }
+
+ public final class EnumColor implements IConstants {
+
+  public final static String BLACK = "schwarz";
+  public final static String WHITE = "weiss";
+
+ }
+
 }
