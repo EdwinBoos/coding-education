fb2c2939b463dbddb1eb7327b43dd9fb90708735
INFLUENZ <edwin.boos@gmx.de>
2019-02-11 13:11:24 +0100

    Create IEnum.java

---
+++ java/patterns/personalpattern/IEnum.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * 
+ * An example for storing multiple 'enum' types in one interface
+ * 
+ * 
+ * 
+ * Created by Edwin Boos ( 11.02.2019 )
+ * with Jsweet see : http://www.jsweet.org/jsweet-live-sandbox/ 
+ * 
+ *
+ */
+package org.jsweet;
+
+
+public final class Run implements IEnums {
+
+ public static void main(String[] args) {
+
+  System.out.println(EnumCar.VW.toString());
+  System.out.println(EnumCar.AUDI.toString());
+  System.out.println(EnumColor.BLACK.toString());
+  System.out.println(EnumColor.WHITE.toString());
+
+ }
+}
+
+
+public interface IEnums {
+
+ public enum EnumCar {
+  VW,
+  AUDI,
+  WINDY,
+  COLD;
+
+  @Override
+  public String toString() {
+   return this.name();
+  }
+ }
+
+
+ public enum EnumColor {
+  BLACK,
+  WHITE;
+
+  @Override
+  public String toString() {
+   return this.name();
+  }
+ }
+
+
+}
