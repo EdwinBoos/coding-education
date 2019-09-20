c0ac4d8ff076968eba84b4e229dcd6dc63b853a6
INFLUENZ <edwin.boos@gmx.de>
2019-09-20 11:13:31 +0200

    Update Run.java

--- java/oop-clock/src/Run.java
+++ java/oop-clock/src/Run.java
@@ -9,10 +9,6 @@ import java.util.Timer;
  *
  */
 public class Run {
-
-
- public static void main(String[] args) {
-
   public static void main(String[] args) {
    final int delay = 0;
    final int period = 1000;
