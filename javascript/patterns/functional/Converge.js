49b6246185aaf6f235f7b2ca2b64e94de463151b
INFLUENZ <edwin.boos@gmx.de>
2019-09-24 15:17:30 +0200

    Print to curry function

--- javascript/patterns/functional/Converge.js
+++ javascript/patterns/functional/Converge.js
@@ -1,11 +1,11 @@
 const person = { lastName : "Foo", age : 33, firstName : "Foo_" };
 const person2 = { lastName : "Foo2", age : 16, firstName : "Foo_2" };
-const print = (isAdult, firstName, lastName) => console.log(`${isAdult} ${firstName} ${lastName}`);  
 const lastName = ({lastName}) => lastName; 
 const age = ({age}) => age;
 const firstName = ({firstName}) => firstName;
 const biggerThan18 = number => (number > 18);
 const isAdult = R.compose(biggerThan18, age);
+const print = R.curry((isAdult, firstName, lastName) => console.log(`${isAdult} ${firstName} ${lastName}`));
 const logTwoTimes = R.converge(console.log, [(text) => text, (text) => text]); 
 const printPerson = R.converge(print, [isAdult, firstName, lastName]);  
 
